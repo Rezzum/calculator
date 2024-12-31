430c330ef6c5ad2d9a36aa80481e46a562eb5660
Jakub Latoch <172971794+Rezzum@users.noreply.github.com>
2024-12-31 12:12:48 +0100

    Implement more partial fixes to logic

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let operator = "";
 let firstNumber;
 let secondNumber;
 let result;
+let isError = false;
+const inputContainer = document.querySelector(".input-container");
+let output = document.querySelector(".output");
 
 function add(a, b) {
     return result = parseFloat(a) + parseFloat(b);
@@ -23,7 +26,7 @@ function divide(a, b) {
 }
 
 function operate(firstNum, secondNum, localOperator) {
-    let result = 0;
+    let result;
 
     try {
         switch(localOperator) {
@@ -33,85 +36,123 @@ function operate(firstNum, secondNum, localOperator) {
                 return result = subtract(firstNum, secondNum);
             case "*":
                 return result = multiply(firstNum, secondNum);
-            case "/":
-                if (secondNum === 0) {
-                    throw new Error("Can't divide by 0");
-                } else {
-                    return result = divide(firstNum, secondNum);
-                }
+            case "÷":
+                return result = divide(firstNum, secondNum);
             default:
-                return result = "An error has occured."
+                throw new Error("Invalid oeprator");
         }
     } catch(error) {
+        isError = true;
         return error.message;
     }
 }
 
-const inputContainer = document.querySelector(".input-container");
-let output = document.querySelector(".output");
+function scrollToEnd() {
+    output.scrollLeft = output.scrollWidth;
+}
 
 inputContainer.addEventListener("click", (event) => {
-    if (event.target.classList.contains("numbers") && operator === "" && event.target.textContent !== "R") {
-        if (firstNumber === undefined || firstNumber === result) {
-            firstNumber = event.target.textContent;
-        } else {
-            firstNumber += event.target.textContent;
-        }
-        output.textContent = firstNumber;
-    } else if (event.target.classList.contains("numbers") && operator !== "" && event.target.textContent !== "R") {
-        if (secondNumber === undefined) {
-            secondNumber = event.target.textContent;
-        } else {
-            secondNumber += event.target.textContent;
-        }
-        output.textContent = firstNumber + operator + secondNumber;
-    }
 
-    if(event.target.classList.contains("operators") && event.target.textContent !== "=") {
-        if (firstNumber === undefined) {
-            firstNumber = 0;
-        }
-        operator = event.target.textContent;
-        output.textContent = firstNumber + operator;
-    }
+    if (isError && event.target.id !== "AC") {
+        return;
+    } else {
+        // TODO: prevent multiple dots
+        if (event.target.classList.contains("numbers") && operator === "" && event.target.textContent !== "R") {
+            
+            // completely broken, fix some other time
+            // if (firstNumber === undefined || firstNumber === result) {
+            //     if (event.target.id = "." && firstNumber === undefined) {
+            //         firstNumber = `0${event.target.textContent}`;
+            // } else {
+            //     firstNumber = event.target.textContent;
+            // } else {
+            //     firstNumber += event.target.textContent;
+            // }
 
-    if (event.target.textContent === "=" && firstNumber !== undefined && operator !== "" && secondNumber !== undefined) {
-        try {
-            result = operate(firstNumber, secondNumber, operator);
-            output.textContent = result;
-            firstNumber = result;
-            operator = "";
-            secondNumber = undefined;
-        } catch (error) {
-            output.textContent = error.message;
-        }
-    }
-
-    if (event.target.textContent === "AC") {
-        output.textContent = "";
-        firstNumber = undefined;
-        secondNumber = undefined;
-        operator = "";
-    }
-
-    if (event.target.id === "Delete") {
-        if (firstNumber !== undefined && operator === "" && secondNumber === undefined) {
-            firstNumber = firstNumber.slice(0, firstNumber.length-1);
+            // if (event.target.id === "." && firstNumber.includes(".")) {
+            //     return;
+            // }
+            
             output.textContent = firstNumber;
-        } else if (firstNumber === "") {
-            firstNumber === undefined;
-        }
+            scrollToEnd();
+        } else if (event.target.classList.contains("numbers") && operator !== "" && event.target.textContent !== "R") {
+            if (event.target.id === "." && secondNumber.includes(".")) {
+                return;
+            }
+            
+            if (secondNumber === undefined) {
+                secondNumber = event.target.textContent;
+            } else {
+                secondNumber += event.target.textContent;
+            }
 
-        if (secondNumber !== undefined) {
-            secondNumber = secondNumber.slice(0, secondNumber.length-1);
             output.textContent = firstNumber + operator + secondNumber;
-        } else if (secondNumber === "") {
-            secondNumber === undefined;
+            scrollToEnd();
         }
 
-        if(operator !== "" && secondNumber === undefined) {
+        if (event.target.classList.contains("operators") && event.target.textContent !== "=" && secondNumber === undefined) {
+            if (firstNumber === undefined) {
+                firstNumber = 0;
+            }
+
+            operator = event.target.textContent;
+            output.textContent = firstNumber + operator;
+            scrollToEnd();
+        }
+
+        if (event.target.textContent === "=" && firstNumber !== undefined && operator !== "" && secondNumber !== undefined) {
+            try {
+                result = operate(firstNumber, secondNumber, operator);
+                
+                if (typeof result === "number" && !isNaN(result)) {
+                    output.textContent = result.toFixed(6).replace(/\.?0+$/, '');
+                    firstNumber = result.toFixed(6).replace(/\.?0+$/, '');
+                } else {
+                    output.textContent = result;
+                }
+
+                scrollToEnd();
+                operator = "";
+                secondNumber = undefined;
+            } catch (error) {
+                output.textContent = error.message;
+            }
+        }
+
+        if (event.target.textContent === "AC") {
+            output.textContent = "";
+            firstNumber = undefined;
+            secondNumber = undefined;
             operator = "";
-            output.textContent = firstNumber;
+            isError = false;
+        }
+
+        if (event.target.id === "Delete") {
+            if (firstNumber !== undefined && operator === "" && secondNumber === undefined) {
+                firstNumber = firstNumber.slice(0, firstNumber.length-1);
+                output.textContent = firstNumber;
+                scrollToEnd();
+                
+                if (firstNumber === "") {
+                    firstNumber = undefined;
+                } 
+            }
+
+            if (operator !== "" && secondNumber === undefined) {
+                operator = "";
+                output.textContent = firstNumber;
+                scrollToEnd();
+            }
+
+            if (secondNumber !== undefined) {
+                secondNumber = secondNumber.slice(0, secondNumber.length-1);
+                output.textContent = firstNumber + operator + secondNumber;
+                scrollToEnd();
+                
+                if (secondNumber === "") {
+                    secondNumber = undefined;
+                }
+            }
         }
     }
 })
